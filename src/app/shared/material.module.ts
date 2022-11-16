ca0ba60023fdb511b09278b4f124ed8817ac56ce
okomojacob <okomo.code@gmail.com>
2022-11-16 16:00:36 +0300

    All material modules :fire: :books: :pencil2:

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { FormsModule }   from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 const modules = [
   MatCardModule,
+  MatIconModule,
+  MatSnackBarModule,
+  MatProgressSpinnerModule,
+  MatDialogModule,
 ];
-
 
 @NgModule({
   declarations: [],
