5a1ea7aa174937fe5b4d5372992e6a1a4a7ac5e1
okomojacob <okomo.code@gmail.com>
2022-11-16 17:08:45 +0300

    Homepage init :fire: :books: :pencil2:

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,6 +1,12 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MaterialModule } from './../../shared/material.module';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +15,13 @@ import { CommonModule } from '@angular/common';
     HomeComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule, 
+    MaterialModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatMenuModule,
+    MatIconModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
