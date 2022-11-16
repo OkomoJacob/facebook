b478fdc96fc375fa0ac07146d2c2a4344fe8a5f7
okomojacob <okomo.code@gmail.com>
2022-11-16 15:58:26 +0300

    Home routing :lock: :books: :pencil2:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { FacebookGuard } from './guards/facebook.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {
@@ -14,6 +15,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   }
 ];
 
