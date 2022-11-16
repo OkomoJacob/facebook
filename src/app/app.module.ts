c8885aaee5312b40d1feeb14a32a4170f294b623
okomojacob <okomo.code@gmail.com>
2022-11-16 14:13:09 +0300

    Page gurad/routing :books: :pencil2:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './shared/material.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
@@ -19,7 +20,10 @@ import { RegisterComponent } from './components/register/register.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    MaterialModule,
+    FormsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
