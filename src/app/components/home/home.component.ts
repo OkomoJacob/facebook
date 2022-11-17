ada6051d1b16b8577ec75b9a67259c13f2da0dc8
okomojacob <okomo.code@gmail.com>
2022-11-17 13:13:15 +0300

    :rocket: Updating homepage images :camera: :books: set-up :hammer:

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,7 +11,6 @@ export class HomeComponent implements OnInit {
   images: any[] = [
     'https://images-na.ssl-images-amazon.com/images/I/51DR2KzeGBL._AC_.jpg',
     'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg',
-    'https://torange.biz/photofx/93/8/light-vivid-colors-fragment-love-background-rain-fall-cover-93412.jpg',
     'https://blog.craigjoneswildlifephotography.co.uk/wp-content/uploads/2017/10/CMJ3288.jpg',
     'https://cdn.pixabay.com/photo/2017/07/18/18/24/dove-2516641_960_720.jpg',
     'https://imgs.search.brave.com/5m4FkVAwFrPzWzlhq6nwzN-HV_WjGwR4QN3dBnUZArE/rs:fit:713:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5o/SkZRQThrMHNPVnBy/c3pCc1hzcU5BSGFF/NyZwaWQ9QXBp',
