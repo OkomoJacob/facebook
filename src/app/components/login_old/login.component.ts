3425b68a9d2d9bd940d84eee34285200cb2bce39
okomojacob <okomo.code@gmail.com>
2022-11-18 17:09:10 +0300

    Old Login page copy :lock:

---
+++ src/app/components/login_old/login.component.ts
@@ -0,0 +1,23 @@
+import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  login(form: NgForm): void{
+    
+  }
+  openRegister(): void{
+    // this.router.navigate(['/register']);
+  }
+
+}
